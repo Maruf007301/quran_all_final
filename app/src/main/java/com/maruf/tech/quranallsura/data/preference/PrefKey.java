0cfef094e3cb481d1b9df3b8a7fd4ca8bc63254f
maruf <rokeya1057@gmail.com>
2023-07-11 21:29:54 +0600

    final

---
+++ app/src/main/java/com/maruf/tech/quranallsura/data/preference/PrefKey.java
@@ -0,0 +1,5 @@
+package com.maruf.tech.quranallsura.data.preference;
+
+public class PrefKey {
+    public static final String APP_PREF_NAME = "tips_app";
+}
---
+++ app/src/main/java/com/maruf/tech/quranallsura/listeners/ListItemClickListener.java
@@ -0,0 +1,7 @@
+package com.maruf.tech.quranallsura.listeners;
+
+import android.view.View;
+
+public interface ListItemClickListener {
+    public void onItemClick(int position, View view);
+}
